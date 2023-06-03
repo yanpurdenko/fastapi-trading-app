1ba9b256c17c93936242e6f2b4bc46b2983fe4bd
Yan Purdenko <108237132+yanpurdenko@users.noreply.github.com>
2023-06-03 19:42:10 +0200

    rename tables and add necessary fields

--- models/models.py
+++ models/models.py
@@ -1,15 +1,14 @@
 from datetime import datetime
 
-import sqlalchemy
-from sqlalchemy import MetaData, Table, Column, Integer, String, JSON, TIMESTAMP, ForeignKey
+from sqlalchemy import MetaData, Table, Column, Integer, String, JSON, TIMESTAMP, ForeignKey, Boolean
 
 metadata = MetaData()
 
 DATABASE_URL = "sqlite:///./py-fastapi-library-management-api.db"
 
 
-roles = Table(
-    "roles",
+role = Table(
+    "role",
     metadata,
     Column("id", Integer, primary_key=True),
     Column("name", String, nullable=False),
@@ -17,13 +16,16 @@ roles = Table(
 
 )
 
-users = Table(
-    "users",
+user = Table(
+    "user",
     metadata,
     Column("id", Integer, primary_key=True),
-    Column("email", String, nullable=False, unique=True),
-    Column("username", String, nullable=False, unique=True),
-    Column("password", String, nullable=False),
+    Column("email", String(length=320), nullable=False, unique=True),
+    Column("username", String(length=255), nullable=False, unique=True),
+    Column("hashed_password", String, nullable=False),
     Column("registered_at", TIMESTAMP, default=datetime.utcnow),
-    Column("role_id", Integer, ForeignKey("roles.id")),
+    Column("role_id", Integer, ForeignKey("role.id")),
+    Column("is_active", Boolean, default=True, nullable=False),
+    Column("is_superuser", Boolean, default=False, nullable=False),
+    Column("is_verified", Boolean, default=False, nullable=False),
 )
